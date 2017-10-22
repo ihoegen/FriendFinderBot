52404797a71291c1bdae7e6c0bd6a311ba6ed407
Shakeel <khanboy411@hotmail.com>
2017-10-22 02:51:37 -0700

    Deleted unused lines.

--- main.go
+++ main.go
@@ -92,5 +92,3 @@ func (log *logger) Critical(args ...interface{})                 { log.Error(arg
 func (log *logger) Criticalf(format string, args ...interface{}) { log.Errorf(format, args...) }
 func (log *logger) Notice(args ...interface{})                   { log.Info(args...) }
 func (log *logger) Noticef(format string, args ...interface{})   { log.Infof(format, args...) }
-
-func getTweets (userName)
