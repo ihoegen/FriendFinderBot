417b101a0117087cd9b1f8b22a72bfaa3726b72c
Ian Hoegen <ianhoegen@gmail.com>
2017-10-22 01:51:24 -0700

    Initial listener

---
+++ Makefile
@@ -0,0 +1,35 @@
+# Copyright 2017 Google Inc. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to writing, software distributed
+# under the License is distributed on a "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+INSTANCE:="friendfinder"
+ZONE:="us-central1-f"
+USER:="ianhoegen"
+
+friendfinder:
+	GOOS=linux go build -o friendfinder
+
+clean:
+	rm -f friendfinder
+
+instance:
+	gcloud compute instances describe --zone $(ZONE) $(INSTANCE) &> /dev/null || \
+	gcloud compute instances create $(INSTANCE) \
+		--zone $(ZONE) --machine-type "f1-micro" \
+		--image "debian-8-jessie-v20170523" --image-project "debian-cloud";
+
+deploy: instance friendfinder
+	gcloud compute scp --zone $(ZONE) friendfinder friendfinder.service $(USER)@$(INSTANCE):~
+	gcloud compute ssh --zone $(ZONE) $(USER)@$(INSTANCE) --command \
+		"sudo mv ~/friendfinder.service /etc/systemd/system/"
+	gcloud compute ssh --zone $(ZONE) $(USER)@$(INSTANCE) --command \
+		"sudo systemctl enable friendfinder && sudo systemctl start friendfinder"
---
+++ main.go
@@ -0,0 +1,76 @@
+// Copyright 2017 Google Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to writing, software distributed
+// under the License is distributed on a "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"net/url"
+	"os"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/Sirupsen/logrus"
+)
+
+var (
+	consumerKey       = getenv("TWITTER_CONSUMER_KEY")
+	consumerSecret    = getenv("TWITTER_CONSUMER_SECRET")
+	accessToken       = getenv("TWITTER_ACCESS_TOKEN")
+	accessTokenSecret = getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+func getenv(name string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		panic("missing required environment variable " + name)
+	}
+	return v
+}
+
+func main() {
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+
+	log := &logger{logrus.New()}
+	api.SetLogger(log)
+
+	stream := api.PublicStreamFilter(url.Values{
+		"track": []string{"@FriendFinderBot"},
+	})
+
+	defer stream.Stop()
+
+	for v := range stream.C {
+		t, ok := v.(anaconda.Tweet)
+		if !ok {
+			log.Warningf("received unexpected value of type %T", v)
+			continue
+		}
+		log.Info(t.User.ScreenName)
+		message := "Thank you for the mention @" + t.User.ScreenName
+		_, err := api.PostTweet(message, url.Values{})
+		if err != nil {
+			log.Errorf("could not post %v: %v", message, err)
+			continue
+		}
+		log.Infof("posted %v", message)
+	}
+}
+
+type logger struct {
+	*logrus.Logger
+}
+
+func (log *logger) Critical(args ...interface{})                 { log.Error(args...) }
+func (log *logger) Criticalf(format string, args ...interface{}) { log.Errorf(format, args...) }
+func (log *logger) Notice(args ...interface{})                   { log.Info(args...) }
+func (log *logger) Noticef(format string, args ...interface{})   { log.Infof(format, args...) }
